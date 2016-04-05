11b282ee3ffa9c058bab1298fc994b742d734d02
Nathan Whitehead <nwhitehe@gmail.com>
2016-04-04 21:38:15 -0700

    Add responses to console.

--- web/js/Console.js
+++ web/js/Console.js
@@ -4,12 +4,20 @@ var Console =
 	var Console = {};
 
 	Console.create = function(container, options) {
+		// container is DOM id of element containing console
+		// Options
+		//		prompt			String for prompt, can contain color escapes
+		//      active			Whether console is accepting input
+		//      banner          Text to show on startup
 		options = options || {};
-		var prompt = options.prompt || "\x1b[33;1m>>>\x1b[0m ";
-		var active = true;
+		options.prompt = options.prompt || "\x1b[33;1m>>>\x1b[0m ";
 		if (options.active === false) {
-			active = false;
+			options.active = false;
+		} else {
+			options.active = true;
 		}
+		options.banner = options.banner || "\x1b[35;1mWelcome to the console!\x1b[0m\r\n\r\n";
+		options.responder = options.responder || function() {};
 
 		var term_elem = document.getElementById(container);
 		var term = new Terminal();
@@ -18,28 +26,41 @@ var Console =
 		term.fit();
 
 		term.prompt = function () {
-		  term.write('\r\n' + prompt);
-		  term.line_length = 0;
+			term.write(options.prompt);
 		};
 		term.line_length = 0;
+		term.input_line = "";
+		term.active = options.active;
 		term.on('key', function (key, ev) {
-		  var printable = (!ev.altKey && !ev.altGraphKey && !ev.ctrlKey && !ev.metaKey);
+			if (!term.active) return;
+			var printable = (!ev.altKey && !ev.altGraphKey && !ev.ctrlKey && !ev.metaKey);
 
-		  if (ev.keyCode === 13) {
-			term.prompt();
-		  } else if (ev.keyCode === 8) {
-			/*
-			 * Do not delete the prompt
-			 */
-			if (term.line_length > 0) {
-			  term.write('\b \b');
-			  term.line_length--;
+			if (ev.keyCode === 13) {
+				term.write('\r\n');
+				var stay_active = options.responder(term.input_line, term);
+				term.line_length = 0;
+				term.input_line = "";
+				if (stay_active == false) {
+					term.active = false;
+				} else {
+					term.prompt();
+				}
+			} else if (ev.keyCode === 8) {
+				// Don't backspace over prompt
+				if (term.line_length > 0) {
+				  term.write('\b \b');
+				  term.line_length--;
+				  term.input_line = term.input_line.substr(0, term.line_length);
+				}
+			} else if (printable) {
+				term.input_line += "" + key;
+				term.line_length++;
+				term.write(key);
 			}
-		  } else if (printable) {
-			term.write(key);
-			term.line_length++;
-		  }
 		});
+		if (options.active) {
+			term.write(options.banner);
+		}
 		term.prompt();
 		return term;
 	}
